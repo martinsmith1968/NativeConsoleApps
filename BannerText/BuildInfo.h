da24f5aa560549c8221a8e490285937c30ebe41c
Martin Smith <martinsmith1968@gmail.com>
2024-12-17 16:28:48 +0000

    Reorganise headers for better separation

--- BannerText/BuildInfo.h
+++ BannerText/BuildInfo.h
@@ -4,4 +4,4 @@
 
 #define BUILD_DATE      TO_STR(__DATE__)
 #define BUILD_TIME      TO_STR(__TIME__)
-#define BUILD_DATETIME  __DATE__ " " __TIME__
+#define BUILD_DATETIME  BUILD_DATE " " BUILD_TIME
