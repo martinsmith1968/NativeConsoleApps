7a65e2ba52bb23695029f1c66d10bab3f8e691c0
Martin Smith <martinsmith1968@gmail.com>
2024-12-19 12:02:33 +0000

    Add sleep timeout

--- PauseN/Options.cpp
+++ PauseN/Options.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include <string>
+#include <chrono>
 #include "../DNX.Utils/StringUtils.h"
 #include "../DNX.App/DNXValueConverter.h"
 #include "../DNX.App/DNXOptionType.h"
@@ -14,11 +15,17 @@ class Options final : public AppOptions
 public:
     Options()
     {
-        AddOption(OptionType::PARAMETER, ValueType::STRING, "m", "message-text", "", "The Text to display", true);
-        AddOption(OptionType::OPTION, ValueType::INT, "t", "timeout", "30", "The timeout to wait for in seconds", false);
+        auto const defaultMessageText = "Press any key to continue (or wait {timeout} seconds) . . . ";
+        auto const defaultTimeout = std::to_string(30);
+        auto const defaultSleep = std::to_string(200);
 
-        SetOptionValue("message-text", "Press any key to continue (or wait {timeout} seconds) . . . ");
-        SetOptionValue("timeout", "30");
+        AddOption(OptionType::PARAMETER, ValueType::STRING, "", "message-text", defaultMessageText, "The Text to display", false, 0);
+        AddOption(OptionType::OPTION, ValueType::INT, "t", "timeout", defaultTimeout, "The timeout to wait for in seconds", false);
+        AddOption(OptionType::OPTION, ValueType::INT, "s", "sleep", defaultSleep, "The timeout to sleep for between checks for in milliseconds", false);
+
+        SetOptionValue("message-text", defaultMessageText);
+        SetOptionValue("timeout", defaultTimeout);
+        SetOptionValue("sleep", defaultSleep);
     }
 
     string GetMessageText()
@@ -31,8 +38,33 @@ public:
         return ValueConverter::ToInt(GetOptionValue("timeout"));
     }
 
+    int GetSleepMilliseconds()
+    {
+        return ValueConverter::ToInt(GetOptionValue("sleep"));
+    }
+
     string GetFormattedMessageText()
     {
-        return ReplaceString(GetMessageText(), "{timeout}", std::to_string(GetTimeoutSeconds()));
+        return ReplaceString(
+            ReplaceString(
+                GetMessageText(),
+                "{timeout}",
+                std::to_string(GetTimeoutSeconds())
+                ),
+            "{sleep}",
+            std::to_string(GetSleepMilliseconds()
+            )
+        );
     }
+
+    void PostParseValidate() override
+    {
+        auto const timeout_time = std::chrono::seconds(GetTimeoutSeconds());
+        auto const sleep_time = std::chrono::milliseconds(GetSleepMilliseconds());
+
+        if (sleep_time >= timeout_time)
+        {
+            AddError("Sleep time must be less than Timeout");
+        }
+    };
 };
