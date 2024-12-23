2f75f7aa12238730067c032e429e530b7f9010e1
Martin Smith <martinsmith1968@gmail.com>
2024-12-23 21:16:54 +0000

    Fix build info header

--- Common/BuildInfo.h
+++ Common/BuildInfo.h
@@ -2,6 +2,6 @@
 
 #include "Macros.h"
 
-#define BUILD_DATE      TO_STR(__DATE__)
-#define BUILD_TIME      TO_STR(__TIME__)
+#define BUILD_DATE      __DATE__
+#define BUILD_TIME      __TIME__
 #define BUILD_DATETIME  BUILD_DATE " " BUILD_TIME
